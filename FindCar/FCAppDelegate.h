3f74f492bfacf857561e86f67f84981fc05983ed
naghceuz <guanchenzhang@gmail.com>
2014-03-21 20:50:33 -0400

    Initial commit from local git

---
+++ FindCar/FCAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  FCAppDelegate.h
+//  FindCar
+//
+//  Created by Owen on 3/21/14.
+//  Copyright (c) 2014 OwenAlex. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface FCAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
